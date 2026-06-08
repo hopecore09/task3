cc7b05d03fc31dea76d71e6617174588e09ddf8c
hopecore <174726408+hopecore09@users.noreply.github.com>
2026-06-08 16:23:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const http = require('http');
 function isNaturalNumber(value) {
     if (value === undefined || value === null || value === '') return false;
     if (!/^\d+$/.test(value)) return false;
-    const num = parseInt(value, 10);
-    return num > 0 && String(num) === value;
+    if (value === '0') return false;
+    return true;
 }
 
 function gcd(a, b) {
@@ -14,7 +14,8 @@ function gcd(a, b) {
 
 http.createServer((req, res) => {
     const myURL = new URL(req.url, `http://${req.headers.host}`);
-    const { x, y } = Object.fromEntries(myURL.searchParams);
+    const x = myURL.searchParams.get('x');
+    const y = myURL.searchParams.get('y');
     
     if (myURL.pathname === '/rx9950x_gmail_com') {
         if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
@@ -22,13 +23,21 @@ http.createServer((req, res) => {
             res.end('NaN');
             return;
         }
-        const a = parseInt(x, 10);
-        const b = parseInt(y, 10);
+        
+        const a = BigInt(x);
+        const b = BigInt(y);
+        const result = (a * b) / gcdBigInt(a, b);
+        
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(String((a * b) / gcd(a, b)));
+        res.end(String(result));
         return;
     }
     
     res.writeHead(404);
     res.end('Not Found');
 }).listen(process.env.PORT || 3000);
+
+function gcdBigInt(a, b) {
+    while (b) { [a, b] = [b, a % b]; }
+    return a;
+}
